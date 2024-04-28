3d49011d7e0166693feb560044e18e0a25574132
AFP <95211548+AditFahrP@users.noreply.github.com>
2024-04-28 07:13:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ hamburgerBtn.addEventListener('click', () => {
 
 const closeOffcanvasMenu = () => {
     document.querySelector('.navbar-menu__offcanvas').classList.remove('open');
-}
+};
 xmarkBtn = document.getElementById('xmarkButton');
 navMenuBtn = document.querySelectorAll('.navbar-menu__list-offcanvas-item');
 xmarkBtn.addEventListener('click', () => {
@@ -13,4 +13,8 @@ xmarkBtn.addEventListener('click', () => {
 });
 for(i = 0; i < navMenuBtn.length; i++) {
     navMenuBtn[i].addEventListener('click', closeOffcanvasMenu);
-}
+};
+var tahunElement = document.getElementById("tahun");
+    var tahunSaatIni = new Date().getFullYear();
+    tahunElement.textContent = tahunSaatIni;
+};

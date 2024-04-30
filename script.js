9d48dfae5d6bdbe754cf0766f0db1c2b75f8a913
adithia <aditpahrudin7@gmail.com>
2024-04-30 08:26:43 +0700

    Update Html & Css
    Update

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ hamburgerBtn.addEventListener('click', () => {
 const closeOffcanvasMenu = () => {
     document.querySelector('.navbar-menu__offcanvas').classList.remove('open');
 };
+
 xmarkBtn = document.getElementById('xmarkButton');
 navMenuBtn = document.querySelectorAll('.navbar-menu__list-offcanvas-item');
 xmarkBtn.addEventListener('click', () => {
     closeOffcanvasMenu();
 });
+
 for(i = 0; i < navMenuBtn.length; i++) {
     navMenuBtn[i].addEventListener('click', closeOffcanvasMenu);
 };
